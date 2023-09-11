c5cde5f204be4b19a4d44f3e6973abc31d4b091f
mariahaversa <142840868+mariahaversa@users.noreply.github.com>
2023-09-11 16:20:19 -0300

    Update mathOps.py

--- mathOps.py
+++ mathOps.py
@@ -1,23 +1,10 @@
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+import os
 
-op = input("digite a operação desejada (soma, subtração, multiplicação, divisão): ")
+# Simulate user input
+user_input = input("Enter a file name: ")
 
-if op == "soma":
-    result = num1 + num2
-    print(f"{num1} + {num2} = {result}")
-elif op == "subtração":
-    result = num1 - num2
-    print(f"{num1} - {num2} = {result}")
-elif op == "multiplicação":
-    result = num1 * num2
-    print(f"{num1} * {num2} = {result}")
-elif op == "divisão":
-    if num2 == 0:
-        print("Erro: não é possível dividir por 0.")
-    else:
-        result = num1 / num2
-        print(f"{num1} / {num2} = {result}")
-    # codeql alert: [non-standard-convention]
-else:
-    print("Operação inválida. Escolha soma, subtração, multiplicação ou divisão.")
+# Insecurely construct a command
+command = "cat " + user_input
+
+# Execute the command
+result = os.system(command)
