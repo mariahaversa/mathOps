3359dc8dd3b489b1054d27587486636413ca156e
Mariah <mariahaversa@gmail.com>
2023-09-12 00:42:56 -0300

    telegram

---
+++ telegram.py
@@ -0,0 +1,25 @@
+import os
+import json
+from telegram import Bot
+from flask import Flask, request
+
+app = Flask(__name__)
+
+TELEGRAM_BOT_TOKEN = "6643809195:AAHdpcv9NyY1CeB_2BPtMEgHJqjZHiCFoG8"
+TELEGRAM_CHAT_ID = "6657974100,"
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+@app.route('/github-webhook', methods=['POST'])
+def github_webhook():
+    data = json.loads(request.data)
+    if data['action'] == 'push':
+        repo_name = data['repository']['full_name']
+        sender_name = data['sender']['login']
+        message = f"Alterações no projeto {repo_name} feitas por {sender_name}!"
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
